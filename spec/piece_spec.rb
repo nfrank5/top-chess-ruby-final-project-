8a1ad3e48034a0c02e16174b9df928f375495ca9
nfrank5 <nfrankenfeld@gmail.com>
2022-11-15 10:33:33 +0800

    Add different_color? method tests

--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -1,10 +1,31 @@
 require_relative '../lib/piece'
+require_relative '../lib/king'
 
 
 describe Piece do
   describe '#different_color?' do
+    subject(:piece_different_color) { described_class.new('white',"\u2655",[7, 3]) }
+    #let(:queen_different_color) { instance_double(Queen) }
+    let(:king_different_color) { instance_double(King) }
+    context 'when the two pieces are different color' do
+      before do
+        #allow(queen_different_color).to receive(:color).and_return('white')
+        allow(king_different_color).to receive(:color).and_return('black')
+      end
+      it 'returns true' do
+        expect(piece_different_color.different_color?(king_different_color)).to be true
+      end
+    end
 
-
+    context 'when the two pieces are same color' do
+      before do
+        #allow(queen_different_color).to receive(:color).and_return('white')
+        allow(king_different_color).to receive(:color).and_return('white')
+      end
+      it 'returns false' do
+        expect(piece_different_color.different_color?(king_different_color)).to be false
+      end
+    end
   end
 
 end
