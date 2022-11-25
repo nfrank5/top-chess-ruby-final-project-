2b836d77979bbd5c1e8cb32a1d25593aeb7cc877
nfrank5 <nfrankenfeld@gmail.com>
2022-11-25 18:21:15 +0800

    Refactoring data module

--- lib/data.rb
+++ lib/data.rb
@@ -3,13 +3,11 @@ require_relative '../lib/input'
 
 module Data
   include Input
-  
+
   def save_games
-    puts 'Do you want to save the game?(Y/N)'
-    ans = player_input(/^[ynYN]$/, 'Only one letter Y or N')
-    if(ans.downcase == 'y')
-      puts 'Enter a name to save the game(3 to 10 letters): '
-      game_name = player_input(/^[a-zA-Z0-9\-_]{3,10}$/, "Only letters, numbers, dash(-) and underscore(_)")
+    ans = confirm_save_game?
+    if ans == 'y'
+      game_name = input_name_save_game
       file = File.open("./lib/saved-games/#{game_name}.yml", 'w')
       file.puts YAML.dump({
                             one: @player_one,
@@ -25,27 +23,28 @@ module Data
   end
 
   def load_game
-    puts 'Do you want to load a saved Game?(Y/N)'
-    ans = player_input(/^[ynYN]$/, 'Input just one letter "Y" for Yes or "N" for No' )
-    return unless ans == 'y'
-    puts 'Saved Games:'
-    Dir.glob('./lib/saved-games/*.yml').each do | file | 
-      puts " - #{file.split('/')[3].split('.')[0]}"
-    end
-    puts 'Write the name of the game you want to load'
-    game_name = player_input(/^[a-zA-Z0-9]{3,10}$/, 'Write the exact name of the game, case sensitive')
+    return unless confirm_load_game?
+    file_found = false
+    print_saved_games
+    game_number = select_number_saved_games
 
-    Dir.glob('./lib/saved-games/*.yml').each do |file|
-      if(file == "./lib/saved-games/#{game_name}.yml")
-        file = YAML.load_file("./lib/saved-games/#{game_name}.yml", aliases: true, permitted_classes: [Symbol, Game, Player, Board, King, Queen, Pawn, Knight, Rook, Bishop])
-        @player_one = file[:one]
-        @player_two = file[:two]
-        @current_board = file[:board]
-        @current_player = file[:current]
-        @other_player = file[:other]
-        break
-      end
+    Dir.glob('./lib/saved-games/*.yml').each_with_index do |file, i|
+      next unless i + 1 == game_number
+
+      file_found = true
+      game_name = file.split('/')[3].split('.')[0]
+      file = YAML.load_file("./lib/saved-games/#{game_name}.yml",
+                            aliases: true,
+                            permitted_classes: [Symbol, Game, Player, Board,
+                                                King, Queen, Pawn, Knight, Rook, Bishop])
+      @player_one = file[:one]
+      @player_two = file[:two]
+      @current_board = file[:board]
+      @current_player = file[:current]
+      @other_player = file[:other]
+      break
     end
-    ans == 'y'
+    puts 'No game Found'
+    file_found
   end
 end
